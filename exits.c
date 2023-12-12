4545783f64cbb40be8f791aef0f6ff1447d3f583
Omar Ahmed <73648173+Omar2454@users.noreply.github.com>
2023-12-13 01:52:32 +0200

    exits.c

--- exits.c
+++ exits.c
@@ -1,43 +1,44 @@
 #include "shell.h"
 
 /**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
- */
+**_strncpy - copies a string
+*@dest: the destination string to be copied to
+*@src: the source string
+*@n: the amount of characters to be copied
+*Return: the concatenated string
+*/
 char *_strncpy(char *dest, char *src, int n)
 {
     char *s = dest;
-
-    for (int i = 0; src[i] != '\0' && i < n - 1; i++)
+    int i = 0;
+    for (i = 0; src[i] != '\0' && i < n - 1; i++)
     {
         dest[i] = src[i];
     }
 
-    for (int j = i; j < n; j++)
+    int j = i;
+    for (j = i; j < n; j++)
     {
         dest[j] = '\0';
     }
 
-    return s;
+    return (s);
 }
 
 /**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
- */
+**_strncat - concatenates two strings
+*@dest: the first string
+*@src: the second string
+*@n: the amount of bytes to be maximally used
+*Return: the concatenated string
+*/
 char *_strncat(char *dest, char *src, int n)
 {
     int i, j;
     char *s = dest;
 
     for (i = 0; dest[i] != '\0'; i++)
-        ; // Increment i until the end of dest
+        ; /* Increment i until the end of dest */
 
     for (j = 0; src[j] != '\0' && j < n; j++, i++)
     {
@@ -47,15 +48,15 @@ char *_strncat(char *dest, char *src, int n)
     if (j < n)
         dest[i] = '\0';
 
-    return s;
+    return (s);
 }
 
 /**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
- */
+**_strchr - locates a character in a string
+*@s: the string to be parsed
+*@c: the character to look for
+*Return: (s) a pointer to the memory area s
+*/
 char *_strchr(char *s, char c)
 {
     for (; *s != '\0'; s++)
@@ -66,4 +67,3 @@ char *_strchr(char *s, char c)
 
     return (*s == c) ? s : NULL;
 }
-
